03ac6efa8efdebe65748b101729689182494f460
sankar3969 <sankar.juvva3@gmail.com>
2024-09-16 22:52:57 +0530

    the working special variables

---
+++ 05_specialVariable.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+
+echo "All variables passed to the script $@"
+
+echo " present script name is $O "
+
+echo "Present working directory $PWD"
+
+echo "Home directory of the present user $HOME"
+
+echo " how to get output of the command $?"
+
+echo "process id for the current shell script $$"
+
+sleep 100 &
+echo "PID of the last background process $!"
