4701c2b86cc3c0dc2889baf1a0f060c5a5f64fd6
sankar3969 <sankar.juvva3@gmail.com>
2024-09-16 22:17:36 +0530

    data types changed files

--- 03_datatypes.sh
+++ 03_datatypes.sh
@@ -11,3 +11,9 @@ SUM=$(($Number1+$Number2))
 
 echo "The Number is $Number1 and $Number2  and $SUM"
 
+if [ $SUM gt 500]
+then
+echo " The sum is greater than 100"
+else
+echo " the sum is less than 100"
+fi
