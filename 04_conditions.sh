70719ffdbb074e5e839002e394d5325974c529c7
sankar3969 <sankar.juvva3@gmail.com>
2024-09-17 01:09:02 +0530

    conditions practice

---
+++ 04_conditions.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+
+USERID=$(id -u)
+
+if($USERID -ne 0)
+then
+echo " this is the super user you can proceed for installation"
+else
+echo "this is not super user plaese procced with super user access"
+exit 1
+fi
