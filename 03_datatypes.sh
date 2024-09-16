ab7c4266e30f70d1d8109d966febb6e67724a8fc
sankar3969 <sankar.juvva3@gmail.com>
2024-09-16 21:52:09 +0530

    data types files

---
+++ 03_datatypes.sh
@@ -0,0 +1,15 @@
+#! bin/bash
+DATE=$(date + %F)
+
+echo "print the today  date $DATE"
+
+
+Number1=$1
+Number2=$2
+
+SUM=$( (Number1+Number2) )
+
+Echo " The reslt is: $?"
+
+Echo "The first number is: $Number1 and The second number is: $Number2  The total is: $SUM "
+
