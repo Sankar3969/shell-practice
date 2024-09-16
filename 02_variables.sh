58c72cdf2ab0fa41374d90263527f0e291eb5c81
sankar3969 <sankar.juvva3@gmail.com>
2024-09-16 20:17:39 +0530

    variables changed

--- 02_variables.sh
+++ 02_variables.sh
@@ -8,17 +8,28 @@ echo "$person2 is already know devops"
 person1=$1
 person2=$2
 
-echo "please enter your first name {$person1}"
-echo "please enter your second name {$person2}"
+echo "please enter your first name ${person1}"
+echo "please enter your second name ${person2}"
 
 fruits=("Apple" "graps" "banana")
 
-echo "the first one on the fruits list is {$fruits[0]}"
-echo "the first one on the fruits list is {$fruits[1]}"
-echo "the first one on the fruits list is {$fruits[2]}"
-echo "the first one on the fruits list is {$fruits[@]}"
+echo "the first one on the fruits list is ${fruits[0]}"
+echo "the first one on the fruits list is ${fruits[1]}"
+echo "the first one on the fruits list is ${fruits[2]}"
+echo "the first one on the fruits list is ${fruits[@]}"
 
-read -s Username
-read -s password
+echo "please enter your the username"
+read  Username
 echo "the first Username is $Username"
+
+echo "please enter your the password"
+read  password
+echo "the password is $password"
+
+echo "please enter your the username without display"
+read -s Username
+echo "the first Username is $Username"
+
+echo "please enter your the password without display"
+read -s password
 echo "the password is $password"
