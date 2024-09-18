86908d1843defdba6ea40ae2603cd1d7a8ecb18d
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 14:47:18 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -8,16 +8,18 @@ value=$?
 # echo "the software value is :$value "
 return $value
 }
-
-for package in $@
-do
 status1=$(install_Status $package)
 echo "the value : $status1"
-# if [ $status1 -ne 0 ]
-# then
-# echo "$1 software not installed please install"
-# else 
-# echo "$1 is already installed software"
-# fi
 
-done
+# for package in $@
+# do
+# status1=$(install_Status $package)
+# echo "the value : $status1"
+# # if [ $status1 -ne 0 ]
+# # then
+# # echo "$1 software not installed please install"
+# # else 
+# # echo "$1 is already installed software"
+# # fi
+
+# done
