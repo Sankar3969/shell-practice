639e179c6359308336b91b4db3609b228e555f31
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 14:26:09 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -12,11 +12,11 @@ for package in $@
 do
 status1=$(install_Status $package)
 echo "status is :$status1"
-if [ $status1 -ne 0 ]
-then
-echo "$1 software not installed please install"
-else 
-echo "$1 is already installed software"
-fi
+# if [ $status1 -ne 0 ]
+# then
+# echo "$1 software not installed please install"
+# else 
+# echo "$1 is already installed software"
+# fi
 
 done
