5bb9592d5ec71be7b9dd5916a510069047e8c308
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 15:07:25 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -6,11 +6,11 @@ dnf list installed $1
  #echo " The status of installation software  $1 is : $?"
 local value=$?
 # echo "the software value is :$value "
-return $value
+echo $value
 }
 install_Status $1
 
-status1=$?
+status1=(install_Status $1)
 echo "the value : $status1"
 
 # for package in $@
