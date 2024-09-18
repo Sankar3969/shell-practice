abdd72060feaf172dc107a9d80ba5a354da85c08
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 14:04:04 +0530

    functionns

---
+++ 06_functions.sh
@@ -0,0 +1,19 @@
+#! bin/bash
+
+install_Status {
+
+dnf list installed $1
+printf " The status of installation software  $1 is : $? "
+status=$?
+return $status
+}
+
+for package in $@
+do
+status=$(install_Status $package)
+if [ status -ne 0 ]
+then
+echo "$1 software not installed please install"
+else 
+echo "$1 is already installed software"
+done
