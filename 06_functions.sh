f2a43519045d1a3aa802cfe96d1bfd6270a85784
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 15:11:19 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -3,7 +3,7 @@
 install_Status () {
 
 dnf list installed $1
- #echo " The status of installation software  $1 is : $?"
+ echo " The status of installation software  $1 is : $?"
 local value=$?
 # echo "the software value is :$value "
 echo $value
