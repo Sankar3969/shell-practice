39f19d82c1d39ca4753fb2e733147cb8c30495be
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 18:46:24 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -22,14 +22,22 @@ echo "the value : $status"
  status1=$(install_Status1 $1)
  #direct convertion from the function 
 mapfile -t output_array < <(install_Status1 $1)
-echo " the values from status array first value ${output_array[0]}"
-echo " the values from status array  second value ${output_array[1]}"
+
+echo " the values from mapfile array first value ${output_array[0]}"
+echo " the values from mapfile array  second value ${output_array[1]}"
+
+readarray -t output_array1 < <(install_Status1 $1)
+echo " the values from readarry first value ${output_array[0]}"
+echo " the values from readarry array  second value ${output_array[1]}"
 
 IFS=$'\n' read -r -d '' -a status_arry <<< "$status1"
 echo " the values from new array first value ${status_arry[0]}"
 echo " the values from new array second  value  ${status_arry[1]}"
 
-
+readarray -t readarray2 <<< "$status1"
+echo " the values from new array first value ${readarray2[0]}"
+echo " the values from new array second  value  ${readarray2[1]}"
+# for loop 
 for element in "${status_arry[@]}";
 do
 echo " The status_arry  from for loop is "$element" "
