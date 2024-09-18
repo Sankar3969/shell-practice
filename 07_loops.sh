ab751eb41a8cfeb4ec7da96cf077fb185d4064c5
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 22:13:45 +0530

    functionns

--- 07_loops.sh
+++ 07_loops.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 R="\e[1;31m" 
 G="\e[1;32m" 
 N="\e[%0m"
-Y="\e[%33m"
+Y="\e[1;33m"
 CHECK_ACCESS()
 {
     
@@ -12,8 +12,24 @@ CHECK_ACCESS()
     echo  -e " $R This is not super user please run with super user $N"
     exit 1
     else
-    echo -e "$G you have super user access you can proceed for installation $N"
+    echo -e "$G you have super user access proceeding for installation $N"
     fi
 } 
 
 CHECK_ACCESS
+
+VALIDATE() {
+if [ $1 -ne 0 ]
+then
+echo " $2 is not installed please proceed to install "
+else 
+echo " $2 is already installed "
+exit 1;
+fi
+}
+
+for package in $@
+do
+dnf list installed $package
+VALIDATE $? "$package"
+done
