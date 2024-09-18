b786c403acc5233e9589f083bff1495d0e236381
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 14:32:41 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -5,13 +5,14 @@ install_Status () {
 dnf list installed $1
 printf " The status of installation software  $1 is : $? "
 status=$?
+echo "the software value is :$status "
 return $status
 }
 
 for package in $@
 do
 status1=$(install_Status $package)
-echo "the software status is :$status1"
+echo "the value : $status1"
 # if [ $status1 -ne 0 ]
 # then
 # echo "$1 software not installed please install"
