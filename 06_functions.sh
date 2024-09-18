46ec582927b372849bd0927ae60f2ec7fa104692
sankar3969 <sankar.juvva3@gmail.com>
2024-09-18 17:40:53 +0530

    functionns

--- 06_functions.sh
+++ 06_functions.sh
@@ -12,7 +12,7 @@ status=$?
 echo "the value : $status"
 
 # the function with return values with echo with no print statement
-
+# reading the function values from while loop
  install_Status1 () {
  dnf list installed $1
  local value1=$?
@@ -20,7 +20,7 @@ echo "the value : $status"
  echo $value1
  }
  status1=$(install_Status1 $1)
-
+echo " the values from status array $status1[0]"
  install_Status1 $1 | while IFS= read -r line;
  do 
  
@@ -31,19 +31,3 @@ echo "the value : $status"
   fi
  #echo "the value1: $status1"
 done
-
-# Function that prints a statement and returns an integer
-my_function() {
-  local value=42
-  echo "This is a printed statement inside the function."
-  return $value  # Return an integer (status code)
-}
-
-# Call the function
-my_function
-
-# Capture the return value (exit status)
-result=$?
-
-# Print the return value
-echo "The returned value is: $result"
