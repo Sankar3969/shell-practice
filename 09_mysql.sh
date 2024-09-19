857d85b69530e9c4fcb54e8107f5e68388bd44d8
sankar3969 <sankar.juvva3@gmail.com>
2024-09-20 03:03:49 +0530

    the mysql commit

--- 09_mysql.sh
+++ 09_mysql.sh
@@ -42,11 +42,29 @@ INSTALLATION_STATUS(){
      fi
 }
 
-INS_STATUS=$(INSTALLATION_STATUS "mysql")
+INS_STATUS=$(INSTALLATION_STATUS "mysql-server")
 
 if [ $INS_STATUS -eq 0 ]
 then
-     echo -e " mysql is not exsisting $Y please procees to install $N" | tee -a $LOG_FILE 
+     echo -e " mysql-server is not exsisting $Y please procees to install $N" | tee -a $LOG_FILE 
+     dnf install mysql-server -y
+     VALIDATE $? "mysql-server"
 else 
-echo -e "$1  $G already installed skipping ... $N " | tee -a $LOG_FILE    
-fi           
+echo -e "mysql-server  $G already installed skipping ... $N " | tee -a $LOG_FILE    
+fi 
+
+systemctl enable mysqld
+VALIDATE $? "mysql enable"
+
+systemctl restart mysqld
+VALIDATE $? "mysql restart"
+
+mysql -h mysql.sankardevops.shop -u root -pExpenseApp@1 -e 'show databases;'
+if [ $? -ne 0 ]
+echo -e " root user password has not set . Please setup password " | tee -a $LOG_FILE   
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "mysql_secure_installation"
+
+else 
+echo -e "mysql-server $R already installed $N " | tee -a $LOG_FILE   
+fi
