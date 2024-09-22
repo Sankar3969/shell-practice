5a8a57e4405b1d8dfb0365b480a30b7cf5f3d9b3
sankar3969 <sankar.juvva3@gmail.com>
2024-09-22 18:06:31 +0530

    disk usage

--- 15_diskUsage.sh
+++ 15_diskUsage.sh
@@ -8,14 +8,14 @@ G="\e[1;32m"
 N="\e[1;0m"
 Y="\e[1;33m"
 
-DISK_MEM_VAL=($($DISK_SPACE | awk -F " " '{print $6F}' | cut -d "%" -f1))
+xfs_values=($(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1))
 
-if [ ! -z $DISK_MEM_VAL ]
+if [ ! -z $xfs_values ]
 then
 while IFS= read -r val1;
 do
 
-echo "the disk value is $DISK_MEM_VAL"
+echo "the disk value is $xfs_values"
 
 echo " val1 is $val1" 
     if [ $val1 -gt $DEFAULT_VAL ]
@@ -27,7 +27,7 @@ echo " val1 is $val1"
     echo "all are less than 5 "
     fi
 
-done <<< $DISK_MEM_VAL
+done <<< $xfs_values
 else
 echo "DISK_MEM_VAL is empty"
 fi
