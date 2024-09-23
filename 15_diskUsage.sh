a4050d055b8670b28a23f8a1d477fb62762a71af
sankar3969 <sankar.juvva3@gmail.com>
2024-09-23 17:02:29 +0530

    git commint for files

--- 15_diskUsage.sh
+++ 15_diskUsage.sh
@@ -3,10 +3,10 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects, it is usually 75
 
-echo "disk usage is  $DISK_USAGE"
+
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-echo "the $line"
+
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
