b0865eb2821ca1b651ebaf1c02cbb31442cd9383
sankar3969 <sankar.juvva3@gmail.com>
2024-09-23 16:47:27 +0530

    git commint for files

--- 15_diskUsage.sh
+++ 15_diskUsage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects, it is usually 75
 
-
+echo "disk usage is  $DISK_USAGE"
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
 echo "the $line"
