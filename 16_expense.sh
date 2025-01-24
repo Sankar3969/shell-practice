a9cb80bc3820a5838fe9ddce05d9e3bd9c8483b5
sankar3969 <sankar.juvva3@gmail.com>
2025-01-25 02:16:39 +0530

    committed

--- 16_expense.sh
+++ 16_expense.sh
@@ -5,7 +5,7 @@ LOG_FOLDER="/var/log/expense"
 FILE_NAME=$(echo $0 | cut -d "." -f1)
 mkdir -p ${LOG_FOLDER}
 echo " The  file name is: ${FILE_NAME} " 
-LOG_FILE="${LOG_FOLDER}/${TIME_STAMP}.log"
+LOG_FILE="${LOG_FOLDER}/${FILE_NAME}-${TIME_STAMP}.log"
 echo " The log file  is: ${LOG_FILE} " 
 
 
