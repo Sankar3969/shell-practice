7e92c9aa0874f94d15098507f82a771ecc476610
sankar3969 <sankar.juvva3@gmail.com>
2025-01-25 02:03:13 +0530

    committed

--- 16_expense.sh
+++ 16_expense.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FOLDER= "/var/log/expense"
 mkdir -p ${LOG_FOLDER}
-LOG_FILE= "${LOG_FOLDER}+${TIME_STAMP}.log"
+LOG_FILE= "${LOG_FOLDER}/${TIME_STAMP}.log"
 echo " The log file  is: ${LOG_FILE} " 
 
 
