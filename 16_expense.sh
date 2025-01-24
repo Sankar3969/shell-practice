a5f1bda84c21d3b97a5c164f8b6a4bf97faaf982
sankar3969 <sankar.juvva3@gmail.com>
2025-01-25 02:30:24 +0530

    committed

--- 16_expense.sh
+++ 16_expense.sh
@@ -9,3 +9,22 @@ LOG_FILE="${LOG_FOLDER}/${FILE_NAME}-${TIME_STAMP}.log"
 echo " The log file  is: ${LOG_FILE} " 
 
 
+CHECK_ACCESS () {
+    if [ $USERID -ne 0 ]
+    then
+    echo " This user is not super user .. plese proceed with super user "
+    exit 1
+
+    else
+    ehco " This is super user please proceed with installation"
+    fi
+}
+USAGE () {
+   if [ $@ -eq 0 ]
+   then
+   echo " please pass the parameters to execute the file 16_expense.sh param 1, param 2.."
+   fi
+}
+
+CHECK_ACCESS
+USAGE
