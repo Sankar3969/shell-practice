1cf719a6e8705afa52e7f8e1728dbf769c18806c
sankar3969 <sankar.juvva3@gmail.com>
2025-01-25 06:07:38 +0530

    committed

---
+++ 18_backuplogs.sh
@@ -0,0 +1,50 @@
+USERID=$(id -u)
+SOURCE_FOLDER=$1
+DEST_FOLDER=$2
+R="\e33m"
+G="\e32m"
+USAGE () {
+    echo " please pass source and destination folders with file name.."
+}
+
+if [ $# -eq  0 ] 
+then
+USAGE 
+fi
+
+if [ ! -d $SOURCE_FOLDER ]
+then
+echo " the source folder not exists"
+else
+echo " the source folder exists"
+fi
+
+if [ ! -d $DEST_FOLDER ]
+then
+echo " the dest folder not exists"
+else
+echo " the dest folder exists"
+fi
+
+FILES=$(find $SOURCE_FOLDER -name "*.log" -mtime -14) 
+
+if [ ! -f $FILES ]
+then
+ZIP_FILE=$(find $SOURCE_FOLDER -name "*.log" -mtime -14 | zip $DEST_FOLDER -@) 
+
+if [ ! -z $ZIP_FILE ]
+then
+echo " zip fileis not created please check"
+else
+echo " backup complete and please initiating for deletion"
+
+while IFS= read -r file
+do
+rm -rf $SOURCE_FOLDER
+done <<< $FILES
+
+fi
+else 
+echo "no file exists in source folder"
+fi
+
